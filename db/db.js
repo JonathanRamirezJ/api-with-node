79d64da219b9b4777bcaf763cd147e07430c0b24
JonathanRamirez <jonajramirez@gmail.com>
2019-06-23 20:23:01 -0500

    Doc: Add initial project structure

---
+++ db/db.js
@@ -0,0 +1,19 @@
+const todos =  [
+  {
+    id: 1,
+    title: "lunch",
+    description: "Go for lunc by 2pm"
+  },
+  {
+    id: 2,
+    title: "lunch 2",
+    description: "Go for lunc by 2pm"
+  },
+  {
+    id: 3,
+    title: "lunch 3",
+    description: "Go for lunc by 2pm"
+  }
+];
+
+module.exports =  todos;
